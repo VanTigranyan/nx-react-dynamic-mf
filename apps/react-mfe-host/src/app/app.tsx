7c8ac56f73acdd7729c600db14e1c4609a59542a
Van Tigranyan <vtigranyan@adobe.com>
2024-08-22 21:25:24 +0400

    Initial commit

---
+++ apps/react-mfe-host/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-mfe-host" />
+    </div>
+  );
+}
+
+export default App;
